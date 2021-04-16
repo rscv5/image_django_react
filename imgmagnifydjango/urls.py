5822c0915e5b89fbdefc9daa485bdb39e52a7bee
rscv5 <renshuchan@163.com>
2021-04-16 16:58:07 +0800

    new start frontend to backend

--- imgmagnifydjango/urls.py
+++
@@ -1,9 +0,0 @@
-from .api import ImageInfoViewSet
-from rest_framework import  routers
-
-routers = routers.DefaultRouter()
-routers.register('uploadimg',ImageInfoViewSet)
-
-# 127.0.0.1:8000/uploadimg
-
-urlpatterns = routers.urls
